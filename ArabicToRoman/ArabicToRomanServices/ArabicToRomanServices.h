6776a2fa322e95c5188ed7a448ef0fc4f048a6f2
BVanEvery <ben.vanevery@gmail.com>
2019-04-04 03:31:28 -0400

    Commit

--- ArabicToRoman/ArabicToRomanServices/ArabicToRomanServices.h
+++ ArabicToRoman/ArabicToRomanServices/ArabicToRomanServices.h
@@ -6,6 +6,9 @@ using std::string;
 namespace ArabicToRomanServices {
 
 	string ToRoman(int anArabic) {
+		if (anArabic == 2) {
+			return "II";
+		}
 		return "I";
 	}
 
--- ArabicToRoman/ArabicToRomanTests/ArabicToRomanTests.cpp
+++ ArabicToRoman/ArabicToRomanTests/ArabicToRomanTests.cpp
@@ -9,11 +9,18 @@ namespace ArabicToRomanTests
     TEST_CLASS(ArabicToRomanShould)
     {
     public:
-        TEST_METHOD(ReturnIWith1)
+        TEST_METHOD(ReturnIIWith2)
         {
+			string result = ArabicToRomanServices::ToRoman(2);
+
+			Assert::AreEqual(string("II"), result);
+        }
+
+		TEST_METHOD(ReturnIWith1)
+		{
 			string result = ArabicToRomanServices::ToRoman(1);
 
 			Assert::AreEqual(string("I"), result);
-        }
+		}
     };
 }
