5239daf0d921220e880c3842f1cb00fa1d0b4006
BVanEvery <ben.vanevery@gmail.com>
2019-04-04 03:54:26 -0400

    Commit

--- ArabicToRoman/ArabicToRomanServices/ArabicToRomanServices.h
+++ ArabicToRoman/ArabicToRomanServices/ArabicToRomanServices.h
@@ -7,15 +7,15 @@ using std::map;
 
 namespace ArabicToRomanServices {
 
-	map<int, string> symbol = {{1000,"M"},{900,"CM"},{500,"D"},{400, "CD"}, 
+	map<int, string> symbol = { {1000,"M"},{900,"CM"},{500,"D"},{400, "CD"},
 								{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},
 								{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},
-								{1,"I"}};
+								{1,"I"} };
 
-	int levels[] = {1000,900,500,400,
+	int levels[] = { 1000,900,500,400,
 					100,90,50,40,
 					10,9,5,4,
-					1};
+					1 };
 
 	string ToRoman(int anArabic) {
 
@@ -33,5 +33,4 @@ namespace ArabicToRomanServices {
 
 		return result;
 	}
-
 }
